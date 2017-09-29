c43b34bcbd02aae9a6a965664e626b43b287c67c
erikzaksf <erik.zak.sf@gmail.com>
2017-09-29 13:44:47 -0700

    add store views and basic routing

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get '/' do
   erb :index
 end
+
+get '/shoes' do
+  @shoes = Shoe.all
+  erb :shoes
+end
+
+
+get '/stores' do
+  @stores = Store.all
+  @stores.each do |store|
+    if store.recipes == []
+      store.destroy
+    end
+  end
+  erb :stores
+end
