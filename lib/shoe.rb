4c74dd60bea59f7c9e2a2f15513e3a8fbaa88e0a
erikzaksf <erik.zak.sf@gmail.com>
2017-10-07 15:24:24 -0700

    add upcase method to shoes and stores

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,14 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many :stores
+  validates_presence_of :name
+  before_save(:upcase_name)
+
+
+
+  def upcase_name
+   chars = self.name.split("")
+   chars[0].upcase!
+   self.name=chars.join("")
+  end
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many :shoes
+  validates_presence_of :name
+  before_save(:upcase_name)
+
+  def upcase_name
+   chars = self.name.split("")
+   chars[0].upcase!
+   self.name=chars.join("")
+  end
 end
